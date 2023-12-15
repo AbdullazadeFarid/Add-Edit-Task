835a4d994e60170d25aeabfdc01afa596379a41b
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-15 16:30:20 +0300

    .

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ add_btn.addEventListener("click",function(){
 
 
 if( input.value.trim()==""){
-input.value=""   // bu o demekdiki inputa nese daxil edib add edenen sora inputdaki yazi silinsin
-alert("ekrana yazi ver")
+    alert("ekrana yazi ver")
+    input.value=""   // bu o demekdiki inputa nese daxil edib add edenen sora inputdaki yazi silinsin
 } else{
     var li = document.createElement("li")  //li yaradirig
     list.appendChild(li)   //li -ni  UL e elave edirik
@@ -31,7 +31,9 @@ alert("ekrana yazi ver")
    newbtn.innerText = "edit" //teze btn ya metn veririk
    li.appendChild(newbtn) //yeni buttona li ye elave edirikki yazilarin yaninda cixsin
 
+
    newbtn.classList.add("btn_new_style") // yeni yaratdigimiz btn da elave style yaratmag ucun yeni yeni 2 ci klass veririk ve scss de cagiirib style veririk    ve ya newbtn.style.backgroundColor="reng" background yerine istediyimiz color yaza bilerik
+   input.value=""   // bu o demekdiki inputa nese daxil edib add edenen sora inputdaki yazi silinsin
 
    newbtn.addEventListener("click" , function(){  //burada yeni btn yeni EDIT e click etdikde neynesin onu qeyd edirkk
     var new_value = prompt("" ,span.textContent ) //EDIT e click etdikde yazilan deyer prompta cixsin
