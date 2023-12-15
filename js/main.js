8c5522d411781969b1d1d25726681619a45d9667
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-15 12:27:01 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+
+var conteiner = document.querySelector(".conteiner")
+
+var input = document.querySelector(".input")
+
+var add_btn = document.querySelector(".add_btn")
+
+var list = document.querySelector(".list")
+
+
+
+add_btn.addEventListener("click",function(){
+
+
+
+if( input.value.trim()==""){
+input.value=""
+alert("ekrana yazi ver")
+} else{
+    var li = document.createElement("li")  //li yaradirig
+    list.appendChild(li)   //li -ni  UL e elave edirik
+
+    var span = document.createElement("span") //span yaradiriq
+   li.appendChild(span)        //li-ye span elave edirik
+
+   span.textContent=input.value //spana qeyd edirikki inputda yazilan deyeri spana menimset
+
+   var newbtn = document.createElement("button") //yeni button yaradirig edit ucun
+   newbtn.classList.add("add_btn") //class adi veririkki ADD buttonunun syyle lerin gotursun
+   newbtn.innerText = "edit" //teze btn ya at veririk
+   li.appendChild(newbtn) //yeni buttona li ye elave edirikki yazilarin yaninda cixsin
+
+   newbtn.classList.add("btn_new_style") // yeni yaratdigimiz btn da elave style yaratmag ucun yeni yeni 2 ci klass veririk ve scss de cagiirib style veririk
+
+   newbtn.addEventListener("click" , function(){  //burada yeni btn yeni EDIT e click etdikde neynesin onu qeyd edirkk
+    var new_value = prompt("" ,span.textContent ) //EDIT e click etdikde yazilan deyer prompta cixsin
+    span.textContent= new_value   //promptda deyisdiyimiz deyeri ekranda yeni spanda gostersin
+
+    if(new_value){
+span.innerText = new_value;
+    }else{
+        alert("Bow saxlama")
+    }
+})
+
+}
+})
+
