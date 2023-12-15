6c2d20b7e294da1415e3288b03b8f3f858c8b595
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-15 16:23:14 +0300

    .

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ add_btn.addEventListener("click",function(){
 
 
 if( input.value.trim()==""){
-input.value=""
+input.value=""   // bu o demekdiki inputa nese daxil edib add edenen sora inputdaki yazi silinsin
 alert("ekrana yazi ver")
 } else{
     var li = document.createElement("li")  //li yaradirig
@@ -28,10 +28,10 @@ alert("ekrana yazi ver")
 
    var newbtn = document.createElement("button") //yeni button yaradirig edit ucun
    newbtn.classList.add("add_btn") //class adi veririkki ADD buttonunun syyle lerin gotursun
-   newbtn.innerText = "edit" //teze btn ya at veririk
+   newbtn.innerText = "edit" //teze btn ya metn veririk
    li.appendChild(newbtn) //yeni buttona li ye elave edirikki yazilarin yaninda cixsin
 
-   newbtn.classList.add("btn_new_style") // yeni yaratdigimiz btn da elave style yaratmag ucun yeni yeni 2 ci klass veririk ve scss de cagiirib style veririk
+   newbtn.classList.add("btn_new_style") // yeni yaratdigimiz btn da elave style yaratmag ucun yeni yeni 2 ci klass veririk ve scss de cagiirib style veririk    ve ya newbtn.style.backgroundColor="reng" background yerine istediyimiz color yaza bilerik
 
    newbtn.addEventListener("click" , function(){  //burada yeni btn yeni EDIT e click etdikde neynesin onu qeyd edirkk
     var new_value = prompt("" ,span.textContent ) //EDIT e click etdikde yazilan deyer prompta cixsin
